5515e3272815670e3793e6e1a7402b14e93b5aee
unknown <anshdotcontact@gmail.com>
2023-09-09 14:17:28 +0530

    Global and local scope

---
+++ 05_basics/01_Scopes.js
@@ -0,0 +1,34 @@
+function one() {
+    const username = "hitesh"
+
+    function two() {
+        const website = "youtube"
+        //console.log(username);
+    }
+    //console.log(website);
+
+      two()
+}
+//one()
+
+if(true) {
+    const username = "Ansh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+//
+
+addOne(7)
+function addOne(num) {
+    return num + 2
+}
+
+//addTwo(5)
+const addTwo = function (num){ //function expression 
+    return num + 2
+}
